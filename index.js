8bf738e0fd54cad8f4aac1b50c260bc145ae5cb4
Glucthu <glucthu@protonmail.com>
2021-09-02 15:16:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function insertNumber(num)
+{
+    let TextInsideLi = document.getElementById("bar-content").innerHTML;
+    console.log(num);
+    console.log(bar);
+    document.getElementById("bar-content").innerHTML = num;
+}
