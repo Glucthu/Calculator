13d36bd98ee9ef03f546a2ad000f9038cebd7bde
Glucthu <53957965+Glucthu@users.noreply.github.com>
2022-02-13 14:47:55 -0300

    changed ui

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let num1 = 0, num2 = 0, operator;
 
 document.getElementById("bar-content").disabled = true;
+document.getElementById("bar-content").value = 0;
 
 function insertNumber(num) {
   if(document.getElementById("bar-content").value != "0") {
@@ -20,7 +21,7 @@ function insertOperator(op) {
     case "/":
       num1 = document.getElementById("bar-content").value;
       operator = op;
-      document.getElementById("bar-content").value = "";
+      document.getElementById("bar-content").value = "0";
       break;
     
     case ".":
@@ -28,7 +29,17 @@ function insertOperator(op) {
       break;
 
     case "C":
-      document.getElementById("bar-content").value = "";
+      document.getElementById("bar-content").value = "0";
+      num1 = 0;
+      num2 = 0;
+      break;
+
+    case "CE":
+      document.getElementById("bar-content").value = "0";
+      break;
+
+    case "+/-":
+      document.getElementById("bar-content").value*=-1;
       break;
 
     case "=":
